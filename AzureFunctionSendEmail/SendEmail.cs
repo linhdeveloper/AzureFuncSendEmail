d1a1b5006cf93a89905936f06c3119c379b81a17
linhdeveloper <linhdeveloper@gmail.com>
2025-06-19 23:59:41 -0700

    Connecting to database , get data from table

--- AzureFunctionSendEmail/SendEmail.cs
+++ AzureFunctionSendEmail/SendEmail.cs
@@ -9,6 +9,8 @@ using Microsoft.Azure.Functions.Worker;
 using MailKit;
 using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Authorization;
+//using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;    
 
 namespace AzureFunctionSendEmail
 {    
@@ -34,6 +36,47 @@ namespace AzureFunctionSendEmail
 
             _logger.LogInformation($"Timer function executed at: {DateTime.Now}");
 
+            #region DATABASE CONNECTION
+
+            // Get connection string from Azure App Settings
+            string connectionString = Environment.GetEnvironmentVariable("SqlConnectionString");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                return new BadRequestObjectResult("SQL connection string is not configured.");
+            }
+
+            string queryResult;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    var command = new SqlCommand("SELECT TOP 1 Name FROM Users", conn);
+                    var reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        queryResult = reader.GetString(0);
+                    }
+                    else
+                    {
+                        queryResult = "No data found.";
+                    }
+                }
+
+                return new OkObjectResult($"Top user: {queryResult}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Database error: {ex.Message}");
+                return new StatusCodeResult(500);
+            }
+
+            #endregion
+
+            #region SEND EMAIL
 
             _logger.LogInformation(LogMessage("1.Start function"));
             var mailData = new MailData();
@@ -51,6 +94,12 @@ namespace AzureFunctionSendEmail
             _logger.LogInformation(LogMessage("4.End function"));
 
             return new OkObjectResult("Email request submitted(V2).");
+
+            #endregion
+
+
+
+
         }
 
         private string LogMessage(string message)
