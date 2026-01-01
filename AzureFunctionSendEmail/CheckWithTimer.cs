e041f891e6314500031db12dd351acab676496d0
linhdeveloper <linhdeveloper@gmail.com>
2026-01-01 14:42:18 -0800

    Updated connection string; added local setting Json file.

---
+++ AzureFunctionSendEmail/CheckWithTimer.cs
@@ -0,0 +1,33 @@
+﻿////using Microsoft.AspNetCore.Http;
+////using Microsoft.AspNetCore.Mvc;
+////using AzureFunctionSendEmail;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Azure.WebJobs;
+//using Microsoft.Extensions.Logging;
+
+//using System;
+
+using System;
+using AzureFunctionSendEmail;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+//using Microsoft.Azure.Functions.Worker;
+
+
+public class TimerFunction
+{
+    private readonly ILogger<SendEmail> _logger;
+    public TimerFunction(ILogger<SendEmail> logger, IMailService mailService)
+    {
+        _logger = logger;        
+    }
+
+    [FunctionName("TimerFunction")]
+    //[Function("TimerFunction")]
+    public static void Run(
+        [TimerTrigger("0 */1 * * * *")] TimerInfo myTimer,
+        ILogger log)
+    {
+        log.LogInformation($"Timer triggered at: {DateTime.Now}");
+    }
+}
